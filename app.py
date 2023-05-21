32486efa9e7786d4a65135cd01687afa08afa25a
abhinavdwivedilko16 <abhinavdwivedilko16@gmail.com>
2023-05-22 01:21:31 +0530

    port

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def predict_datapoint():
         return render_template('result.html',final_result=results)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=8080)
